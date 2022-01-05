a05aa6aa88278dc8312e5921c581c7de426761b6
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2022-01-05 15:37:24 +0100

    refactor methods  withing App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,14 @@ import React, { useState } from "react";
 function App() {
   const [input, setInput] = useState("");
   const [items, setItems] = useState([]);
+
   function handleInput(event) {
     return setInput(event.target.value);
   }
 
   function handleClick(event) {
-    return setItems([...items, input]);
+    setItems([...items, input]);
+    setInput("");
   }
 
   return (
@@ -24,9 +26,9 @@ function App() {
       </div>
       <div>
         <ul>
-          {items.map((item) => {
-            return <li>{item}</li>;
-          })}
+          {items.map((item) => (
+            <li>{item}</li>
+          ))}
         </ul>
       </div>
     </div>
